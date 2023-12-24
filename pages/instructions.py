ce6abf5b331673afeebdf8c6150e23cc8d9dc613
Sanskar Shivhare <110665348+Sanskar-Shivhare@users.noreply.github.com>
2023-12-24 20:42:04 +0530

    Add files via upload

---
+++ pages/instructions.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+st.title('~~~ Instructions To Use This App ~~~')
+
+st.header('1. upload your resume through ulpoad your pdf option ')
+st.header('2. our app will show you the whole text of your resume ')
+st.header('3. after seeing that order/hierarchy of your resume ')
+st.header('4. enter section in section options')
+st.header('5. for example - you resume order is summary, experience, education, skills')
+st.subheader('- so you will enter summary in first section')
+st.subheader('- experience in second section')
+st.subheader('- education in third section')
+st.subheader('- skills in forth section')
+st.subheader('- and when your resume got ended enter the last word of your resume in the last section')
+st.subheader('- for example last word of your resume is `members` so you will enter this in next section')
+st.header('6. enter job discription in the job description option press enter')
+st.header('7. than click on find score,it will show you the result')
+
+
+st.title('~~~~~ WARNINGS ~~~~~')
+st.subheader('if you have resume section like awards/certificate or my work')
+st.subheader('than in section enter first word of them like - awards,my')
